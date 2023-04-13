83bffd2a8f1bb82f3afc88f8f761e659db33f0f0
mwangli <1325533186@qq.com>
2023-04-13 10:48:30 +0800

    redis配置修改

--- src/main/java/online/mwang/foundtrading/config/WebConfig.java
+++ src/main/java/online/mwang/foundtrading/config/WebConfig.java
@@ -18,6 +18,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(loginInterceptor).excludePathPatterns("/login/account");
+        registry.addInterceptor(loginInterceptor).excludePathPatterns("/login/account","/test");
     }
 }
