0aaed510d9887baa2d5249f8b2188520e4346aba
mwangli <1325533186@qq.com>
2023-04-21 11:57:02 +0800

    CI部署测试2

--- src/main/java/online/mwang/foundtrading/controller/TestController.java
+++ src/main/java/online/mwang/foundtrading/controller/TestController.java
@@ -23,7 +23,7 @@ public class TestController {
     @SneakyThrows
     @GetMapping("test")
     public String test() {
-        return "test:v1.3.5";
+        return "test:v1.3.6";
     }
 
     @SneakyThrows
