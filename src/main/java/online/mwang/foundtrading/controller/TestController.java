c245eb3c157230bae9e1751ec7fb96cb724701cc
mwangli <1325533186@qq.com>
2023-04-21 22:37:05 +0800

    v2.9

--- src/main/java/online/mwang/foundtrading/controller/TestController.java
+++ src/main/java/online/mwang/foundtrading/controller/TestController.java
@@ -29,7 +29,7 @@ public class TestController {
     @SneakyThrows
     @GetMapping("test")
     public String test() {
-        return "test:v2.8";
+        return "test:v2.9";
     }
 
     @SneakyThrows
