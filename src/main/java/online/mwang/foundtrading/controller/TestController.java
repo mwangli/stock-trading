65952fc60324396cc7441d398abd3c828c7b6461
mwangli <1325533186@qq.com>
2023-04-21 09:13:22 +0800

    CI部署测试

--- src/main/java/online/mwang/foundtrading/controller/TestController.java
+++ src/main/java/online/mwang/foundtrading/controller/TestController.java
@@ -1,8 +1,13 @@
 package online.mwang.foundtrading.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
 
 /**
  * @version 1.0.0
@@ -17,5 +22,11 @@ public class TestController {
     @GetMapping("test")
     public String test() {
         return "test:v1.2.4";
+    }
+
+    @PostMapping("imageUpdate")
+    public String imageUpdate(@RequestBody HashMap<String,Object> params) {
+        log.info(JSONObject.toJSONString(params));
+        return "success";
     }
 }
