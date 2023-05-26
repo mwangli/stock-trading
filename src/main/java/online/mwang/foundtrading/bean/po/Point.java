a42983e57cd31ded1a7b7b317ad5d29c763f96ba
mwangli <1325533186@qq.com>
2023-05-26 16:17:39 +0800

    数据分析接口

---
+++ src/main/java/online/mwang/foundtrading/bean/po/Point.java
@@ -0,0 +1,19 @@
+package online.mwang.foundtrading.bean.po;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @version 1.0.0
+ * @author: mwangli
+ * @date: 2023/5/26 15:13
+ * @description: Point
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Point {
+    private String x;
+    private String y;
+}
--- src/main/java/online/mwang/foundtrading/controller/FoundTradingController.java
+++ src/main/java/online/mwang/foundtrading/controller/FoundTradingController.java
@@ -4,14 +4,21 @@ import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.ObjectUtils;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import lombok.RequiredArgsConstructor;
 import online.mwang.foundtrading.bean.base.Response;
+import online.mwang.foundtrading.bean.po.AccountInfo;
+import online.mwang.foundtrading.bean.po.AnalysisData;
 import online.mwang.foundtrading.bean.po.FoundTradingRecord;
+import online.mwang.foundtrading.bean.po.Point;
 import online.mwang.foundtrading.bean.query.FoundTradingQuery;
+import online.mwang.foundtrading.mapper.AccountInfoMapper;
 import online.mwang.foundtrading.service.FoundTradingService;
-import org.springframework.beans.factory.annotation.Autowired;
+import online.mwang.foundtrading.utils.DateUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @version 1.0.0
@@ -21,11 +28,11 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("foundTrading")
+@RequiredArgsConstructor
 public class FoundTradingController {
 
-    @Autowired
     private FoundTradingService foundTradingService;
-
+    private AccountInfoMapper accountInfoMapper;
 
     @PostMapping
     public Boolean createFound(@RequestBody FoundTradingRecord foundTradingRecord) {
@@ -55,5 +62,37 @@ public class FoundTradingController {
                 .orderBy(true, "ascend".equals(query.getSortOrder()), FoundTradingRecord.getOrder(query.getSortKey()));
         Page<FoundTradingRecord> pageResult = foundTradingService.page(page, queryWrapper);
         return Response.success(pageResult.getRecords(), pageResult.getTotal());
+    }
+
+    @GetMapping("analysis")
+    public Response<AnalysisData> analysis() {
+        final AnalysisData data = new AnalysisData();
+        // 获取所有已卖出的股票，按更新时间倒序
+        final List<FoundTradingRecord> sortedSoldList = foundTradingService.list(new QueryWrapper<FoundTradingRecord>().lambda().eq(FoundTradingRecord::getSold, "1"));
+        // 获取最近收益金额
+        sortedSoldList.stream().findFirst().ifPresent(o -> data.setIncome(o.getIncome()));
+        // 获取上次收益金额
+        sortedSoldList.stream().skip(1).findFirst().ifPresent(o -> data.setPreIncome(o.getIncome()));
+        // 获取累计收益
+        data.setTotalIncome(sortedSoldList.stream().mapToDouble(FoundTradingRecord::getIncome).reduce(Double::sum).orElse(0.0));
+        // 获取平均收益
+        data.setAvgIncome(data.getTotalIncome() / sortedSoldList.size());
+        // 获取日收益率
+        sortedSoldList.stream().findFirst().ifPresent(o -> data.setDailyIncomeRate(o.getDailyIncomeRate()));
+        // 查询账户金额
+        accountInfoMapper.selectList(new QueryWrapper<AccountInfo>().lambda().orderByDesc(AccountInfo::getUpdateTime)).stream().findFirst().ifPresent(data::setAccountInfo);
+        // 查询收益列表
+        data.setIncomeList(sortedSoldList.stream().limit(10).map(o -> new Point(DateUtils.dateFormat.format(o.getSaleDate()), o.getIncome().toString())).sorted(Comparator.comparing(Point::getX)).collect(Collectors.toList()));
+        // 查询收益率列表
+        data.setRateList(sortedSoldList.stream().limit(10).map(o -> new Point(DateUtils.dateFormat.format(o.getSaleDate()), String.format("%.4f",o.getIncome()/o.getBuyAmount()))).sorted(Comparator.comparing(Point::getX)).collect(Collectors.toList()));
+        // 查询日收益率列表
+        data.setDailyRateList(sortedSoldList.stream().limit(10).map(o -> new Point(DateUtils.dateFormat.format(o.getSaleDate()), o.getDailyIncomeRate().toString())).sorted(Comparator.comparing(Point::getX)).collect(Collectors.toList()));
+        // 收益排行
+        data.setIncomeOrder(sortedSoldList.stream().sorted(Comparator.comparing(FoundTradingRecord::getIncome).reversed()).limit(7).map(o->new Point(o.getCode()+o.getName(),o.getIncome().toString())).collect(Collectors.toList()));
+        // 日收益率牌型
+        data.setDailyRateOrder(sortedSoldList.stream().sorted(Comparator.comparing(FoundTradingRecord::getDailyIncomeRate).reversed()).limit(7).map(o->new Point(o.getCode()+o.getName(),o.getDailyIncomeRate().toString())).collect(Collectors.toList()));
+        // 持有天數排行
+        data.setHoldDaysList(sortedSoldList.stream().sorted(Comparator.comparing(FoundTradingRecord::getHoldDays).reversed()).limit(7).map(o->new Point(o.getCode()+o.getName(),o.getHoldDays().toString())).collect(Collectors.toList()));
+        return Response.success(data);
     }
 }
