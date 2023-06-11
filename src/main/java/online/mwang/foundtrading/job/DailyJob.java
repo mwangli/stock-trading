c567216ae7d2beb5641ecc715d85c9ad3f67a312
mwangli <1325533186@qq.com>
2023-06-11 19:41:15 +0800

    token超时优化

--- src/main/java/online/mwang/foundtrading/job/DailyJob.java
+++ src/main/java/online/mwang/foundtrading/job/DailyJob.java
@@ -220,10 +220,12 @@ public class DailyJob {
             map.put(errorNo, res.getString("ERRORMESSAGE"));
             if (errorCodes.contains(errorNo)) {
                 info.setPermission("0");
+            } else {
+                info.setPermission("1");
             }
-            log.info("刷新当前股票[{}-{}]交易权限。", info.getCode(), info.getName());
+            log.info("刷新当前股票[{}-{}]交易权限: {}", info.getCode(), info.getName(), info.getPermission());
         });
-        log.info("错误码合集：{}", map);
+        log.info("交易权限错误码合集：{}", map);
         saveDate(stockInfos);
         // 取消所有提交的订单
         cancelAllOrder();
