8c8e9d5f7d38a60fbf2c8083406187547cd96a6b
mwangli <1325533186@qq.com>
2023-07-14 17:30:02 +0800

    Demo工具

--- src/main/java/online/mwang/foundtrading/controller/LoginController.java
+++ src/main/java/online/mwang/foundtrading/controller/LoginController.java
@@ -1,5 +1,6 @@
 package online.mwang.foundtrading.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
@@ -50,9 +51,22 @@ public class LoginController {
         log.info("username is {}, password is {}", param.getUsername(), param.getPassword());
         final String monthDate = new SimpleDateFormat(SDF).format(new Date());
         final String reverseDate = new StringBuilder(monthDate).reverse().toString();
-        if (USERNAME.equalsIgnoreCase(param.getUsername()) && reverseDate.equals(param.getPassword())) {
+        if ("demo".equals(param.getUsername())){
             final String token = generateToken();
-            stringRedisTemplate.opsForValue().set(token, "", TOKEN_EXPIRE_HOURS, TimeUnit.HOURS);
+            JSONObject user = new JSONObject();
+            user.put("name", "demo");
+            user.put("avatar", "https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png");
+            user.put("access", "demo");
+            stringRedisTemplate.opsForValue().set(token, JSON.toJSONString(user), TOKEN_EXPIRE_HOURS, TimeUnit.HOURS);
+            return Response.success(token);
+        }
+        if  ( USERNAME.equalsIgnoreCase(param.getUsername()) && reverseDate.equals(param.getPassword())) {
+            final String token = generateToken();
+            JSONObject user = new JSONObject();
+            user.put("name", "admin");
+            user.put("avatar", "https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png");
+            user.put("access", "admin");
+            stringRedisTemplate.opsForValue().set(token, JSON.toJSONString(user), TOKEN_EXPIRE_HOURS, TimeUnit.HOURS);
             return Response.success(token);
         } else {
             return Response.fail(1101, "用户名或密码错误!");
@@ -68,10 +82,13 @@ public class LoginController {
 
     @SneakyThrows
     @GetMapping("/currentUser")
-    public Response<JSONObject> currentUser(LoginParam param) {
-        JSONObject user = new JSONObject();
-        user.put("name", "Admin");
-        user.put("avatar", "https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png");
-        return Response.success(user);
+    public Response<JSONObject> currentUser(HttpServletRequest request) {
+        final String token = request.getHeader("token");
+        final String user = stringRedisTemplate.opsForValue().get(token);
+//        JSONObject user = new JSONObject();
+//        user.put("name", "Admin");
+//        user.put("avatar", "https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png");
+//        user.put("access", "admin");
+        return Response.success(JSON.parseObject(user));
     }
 }
