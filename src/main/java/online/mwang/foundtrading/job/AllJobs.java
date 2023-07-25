d357af5fd7363058172ae4f58b638c450152c134
mwangli <1325533186@qq.com>
2023-07-25 11:05:14 +0800

    买入随机范围优化

--- src/main/java/online/mwang/foundtrading/job/AllJobs.java
+++ src/main/java/online/mwang/foundtrading/job/AllJobs.java
@@ -59,7 +59,7 @@ public class AllJobs {
     private static final int LOGIN_RETRY_TIMES = 10;
     private static final double PRICE_TOTAL_FALL_LIMIT = -5.0;
     private static final double PRICE_TOTAL_UPPER_LIMIT = 6.0;
-    private static final int BUY_RETRY_LIMIT = 20;
+    private static final int BUY_RETRY_LIMIT = 100;
     private static final int WAIT_TIME_SECONDS = 10;
     private static final int HISTORY_PRICE_LIMIT = 100;
     private static final int UPDATE_BATCH_SIZE = 500;
