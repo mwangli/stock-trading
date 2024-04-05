42599db897adce09a9e15cde12470cdf21d3899a
mwangli <1325533186@qq.com>
2024-04-05 18:08:26 +0800

    int

---
+++ src/main/java/online/mwang/stockTrading/predict/model/ModelConfig.java
@@ -0,0 +1,81 @@
+package online.mwang.stockTrading.predict.model;
+
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.nn.conf.BackpropType;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.LSTM;
+import org.deeplearning4j.nn.conf.layers.RnnOutputLayer;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.learning.config.Nesterovs;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by zhanghao on 26/7/17.
+ *
+ * @author ZHANG HAO
+ */
+@Component
+public class ModelConfig {
+
+
+    private static final double learningRate =  0.05;
+    private static final int seed = 1024;
+    private static final int lstmLayer1Size = 128;
+    private static final int lstmLayer2Size = 128;
+    private static final int denseLayerSize = 16;
+    private static final double dropoutRatio = 0.1;
+    private static final int truncatedBPTTLength = 10;
+
+
+    public static MultiLayerNetwork buildLstmNetworks(int nIn, int nOut) {
+        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+                .seed(seed)
+                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+                .weightInit(WeightInit.XAVIER)
+                .updater(new Nesterovs(learningRate, 0.9))
+                .l2(1e-4)
+                .list()
+                .layer(0, new LSTM.Builder()
+                        .nIn(nIn)
+                        .nOut(lstmLayer1Size)
+                        .activation(Activation.TANH)
+                        .gateActivationFunction(Activation.HARDSIGMOID)
+                        .dropOut(dropoutRatio)
+                        .build())
+                .layer(1, new LSTM.Builder()
+                        .nIn(lstmLayer1Size)
+                        .nOut(lstmLayer2Size)
+                        .activation(Activation.TANH)
+                        .gateActivationFunction(Activation.HARDSIGMOID)
+                        .dropOut(dropoutRatio)
+                        .build())
+                .layer(2, new DenseLayer.Builder()
+                        .nIn(lstmLayer2Size)
+                        .nOut(denseLayerSize)
+                        .activation(Activation.RELU)
+                        .build())
+                .layer(3, new RnnOutputLayer.Builder()
+                        .nIn(denseLayerSize)
+                        .nOut(nOut)
+                        .activation(Activation.IDENTITY)
+                        .lossFunction(LossFunctions.LossFunction.MSE)
+                        .build())
+                .backpropType(BackpropType.TruncatedBPTT)
+                .tBPTTForwardLength(truncatedBPTTLength)
+                .tBPTTBackwardLength(truncatedBPTTLength)
+                .pretrain(false)
+                .backprop(true)
+                .build();
+
+        MultiLayerNetwork net = new MultiLayerNetwork(conf);
+        net.init();
+        net.setListeners(new ScoreIterationListener(10));
+        return net;
+    }
+}
