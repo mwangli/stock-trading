138a6bfb4f613e06531bcaa8726a87460c418454
mwangli <1325533186@qq.com>
2024-04-10 16:41:22 +0800

    clean job fix

---
+++ src/main/java/online/mwang/stockTrading/web/controller/OrderInfoController.java
@@ -0,0 +1,41 @@
+package online.mwang.stockTrading.web.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.ObjectUtils;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import online.mwang.stockTrading.web.bean.base.Response;
+import online.mwang.stockTrading.web.bean.po.OrderInfo;
+import online.mwang.stockTrading.web.bean.po.StockInfo;
+import online.mwang.stockTrading.web.bean.query.StockInfoQuery;
+import online.mwang.stockTrading.web.service.OrderInfoService;
+import online.mwang.stockTrading.web.service.StockInfoService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @version 1.0.0
+ * @author: mwangli
+ * @date: 2023/3/20 10:56
+ * @description: FoundTradingController
+ */
+@Slf4j
+@RestController
+@RequestMapping("/orderInfo")
+@RequiredArgsConstructor
+public class OrderInfoController {
+
+    private final static String ASCEND = "ascend";
+    private final OrderInfoService orderInfoService;
+
+    @GetMapping("/list")
+    public Response<List<OrderInfo>> listStockInfo(StockInfoQuery query) {
+        final List<OrderInfo> list = orderInfoService.list();
+        return Response.success(list);
+    }
+}
