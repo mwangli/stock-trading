dd4cd87eadce745a38184b6a5d158c9d012fb155
mwangli <1325533186@qq.com>
2024-04-29 09:44:17 +0800

    fix

--- src/main/java/online/mwang/stockTrading/schedule/jobs/RunPermissionJob.java
+++ src/main/java/online/mwang/stockTrading/schedule/jobs/RunPermissionJob.java
@@ -34,7 +34,7 @@ public class RunPermissionJob extends BaseJob {
         List<String> errorCodes = Arrays.asList("[251112]", "[251127]", "[251299]", "该股票是退市");
         List<StockInfo> stockInfos = stockInfoService.list();
         final HashSet<String> set = new HashSet<>();
-        List<StockInfo> stockInfoList = stockInfos.stream().peek(info -> {
+        stockInfos.forEach(info -> {
             JSONObject result = dataService.buySale("B", info.getCode(), 100.0, 100.0);
             final String message = result.getString("ERRORMESSAGE");
             set.add(message);
@@ -43,10 +43,9 @@ public class RunPermissionJob extends BaseJob {
             } else {
                 info.setPermission("1");
             }
-        }).collect(Collectors.toList());
-        stockInfoService.updateBatchById(stockInfoList);
-        log.info("交易权限错误信息合集:{}", set);
+        });
         stockInfoService.updateBatchById(stockInfos);
+        log.info("交易权限错误信息合集:{}", set);
         // 取消所有提交的订单
         dataService.cancelAllOrder();
     }
