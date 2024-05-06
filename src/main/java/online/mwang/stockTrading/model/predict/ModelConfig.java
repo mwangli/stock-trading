870a72518de0ef525de974e6cceb52eb737a7b6f
mwangli <1325533186@qq.com>
2024-05-06 15:51:21 +0800

    bug-fix3

--- src/main/java/online/mwang/stockTrading/model/predict/ModelConfig.java
+++ src/main/java/online/mwang/stockTrading/model/predict/ModelConfig.java
@@ -27,7 +27,7 @@ public class ModelConfig {
                 .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
                 .weightInit(WeightInit.XAVIER).updater(new Nesterovs(0.0015, 0.9))
                 .list()
-                .layer(new LSTM.Builder().activation(Activation.TANH).nOut(128).gateActivationFunction(Activation.SIGMOID).dropOut(0.2).nIn(inputNum).build())
+                .layer(new LSTM.Builder().activation(Activation.TANH).nIn(inputNum).nOut(128).gateActivationFunction(Activation.SIGMOID).dropOut(0.2).build())
                 .layer(new RnnOutputLayer.Builder(LossFunctions.LossFunction.MSE).activation(Activation.IDENTITY).nOut(outNum).build())
                 .build();
         MultiLayerNetwork net = new MultiLayerNetwork(conf);
