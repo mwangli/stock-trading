dcb19cc680967624140dd06317eb734805caa394
mwangli <1325533186@qq.com>
2024-05-11 19:17:45 +0800

    补充sql文件

--- src/main/resources/sql/found_trading.sql
+++ src/main/resources/sql/found_trading.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80027
  File Encoding         : 65001
 
- Date: 05/04/2024 17:26:12
+ Date: 11/05/2024 19:16:01
 */
 
 SET NAMES utf8mb4;
@@ -29,34 +29,49 @@ CREATE TABLE `account_info`  (
   `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
   `update_time` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 987 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '账户金额表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1007 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '账户金额表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Table structure for code_message
+-- Table structure for model_info
 -- ----------------------------
-DROP TABLE IF EXISTS `code_message`;
-CREATE TABLE `code_message`  (
+DROP TABLE IF EXISTS `model_info`;
+CREATE TABLE `model_info`  (
   `id` bigint(0) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键ID',
-  `code` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_german2_ci NULL DEFAULT NULL COMMENT '错误码',
-  `message` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_german2_ci NULL DEFAULT NULL,
-  `solution` text CHARACTER SET utf8mb4 COLLATE utf8mb4_german2_ci NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 347 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_german2_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Table structure for predict_price
--- ----------------------------
-DROP TABLE IF EXISTS `predict_price`;
-CREATE TABLE `predict_price`  (
-  `id` bigint(0) NOT NULL COMMENT '主键ID',
-  `stock_code` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_german2_ci NULL DEFAULT NULL COMMENT '股票代码',
-  `date` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_german2_ci NULL DEFAULT NULL COMMENT '预测日期',
-  `actual_price` decimal(10, 2) NULL DEFAULT NULL COMMENT '实际价格',
-  `predict_price` decimal(10, 2) NULL DEFAULT NULL COMMENT '预测价格',
+  `code` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '股票代码',
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '模型名称',
+  `train_period` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '训练时长(分钟)',
+  `train_times` int(0) NULL DEFAULT NULL COMMENT '训练次数',
+  `test_deviation` double(32, 8) NULL DEFAULT NULL COMMENT '测试集误差',
+  `score` double(32, 4) NULL DEFAULT NULL COMMENT '模型评分，根据模型在测试集和验证集上的误差和稳定性做出综合评分',
+  `status` int(0) NULL DEFAULT NULL COMMENT '模型状态',
   `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
   `update_time` datetime(0) NULL DEFAULT NULL COMMENT '修改时间',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_german2_ci ROW_FORMAT = Dynamic;
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `idx_name`(`name`) USING BTREE COMMENT '模型名称'
+) ENGINE = InnoDB AUTO_INCREMENT = 7885 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '模型信息表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for order_info
+-- ----------------------------
+DROP TABLE IF EXISTS `order_info`;
+CREATE TABLE `order_info`  (
+  `id` bigint(0) NOT NULL AUTO_INCREMENT COMMENT '主键Id',
+  `answer_no` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_german2_ci NULL DEFAULT NULL COMMENT '订单编号',
+  `code` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_german2_ci NULL DEFAULT NULL COMMENT '股票代码',
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_german2_ci NULL DEFAULT NULL COMMENT '股票名称',
+  `status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_german2_ci NULL DEFAULT NULL COMMENT '交易状态',
+  `date` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_german2_ci NULL DEFAULT NULL COMMENT '交易日期',
+  `time` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_german2_ci NULL DEFAULT NULL COMMENT '交易时间',
+  `type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_german2_ci NULL DEFAULT NULL COMMENT '交易类型',
+  `number` decimal(10, 2) NULL DEFAULT NULL COMMENT '交易数量',
+  `price` decimal(10, 2) NULL DEFAULT NULL COMMENT '交易价格',
+  `amount` decimal(10, 2) NULL DEFAULT NULL COMMENT '订单金额',
+  `peer` decimal(10, 2) NULL DEFAULT NULL COMMENT '手续费',
+  `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime(0) NULL DEFAULT NULL COMMENT '修改时间',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `idx_answer_no`(`answer_no`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1290 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_german2_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for quartz_job
@@ -76,24 +91,7 @@ CREATE TABLE `quartz_job`  (
   `update_time` datetime(0) NULL DEFAULT NULL COMMENT '修改时间',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `name`(`name`) USING BTREE COMMENT '唯一名称'
-) ENGINE = InnoDB AUTO_INCREMENT = 19 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '定时任务表' ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Table structure for score_strategy
--- ----------------------------
-DROP TABLE IF EXISTS `score_strategy`;
-CREATE TABLE `score_strategy`  (
-  `id` bigint(0) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键ID',
-  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '策略名称',
-  `params` varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '策略参数',
-  `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '参数说明',
-  `status` int(0) NULL DEFAULT NULL COMMENT '选中状态',
-  `sort` int(0) NULL DEFAULT NULL COMMENT '排序字段',
-  `deleted` int(0) NULL DEFAULT 1 COMMENT '删除状态',
-  `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
-  `update_time` datetime(0) NULL DEFAULT NULL COMMENT '修改时间',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 17 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '评分策略表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 27 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '定时任务表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for stock_info
@@ -105,18 +103,17 @@ CREATE TABLE `stock_info`  (
   `code` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '股票代码',
   `market` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '上市地区',
   `increase` double(32, 4) NULL DEFAULT NULL COMMENT '增长幅度',
-  `price` double(10, 2) NULL DEFAULT NULL COMMENT '今日价格',
-  `prices` json NULL COMMENT '历史价格',
-  `increase_rate` json NULL COMMENT '日增长率',
+  `price` double(10, 2) NULL DEFAULT NULL COMMENT '当前价格',
+  `predict_price` decimal(10, 2) NULL DEFAULT NULL COMMENT '预测价格',
   `score` double(32, 4) NULL DEFAULT NULL COMMENT '评价得分',
   `permission` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '交易权限',
   `buy_sale_count` int(0) NULL DEFAULT NULL COMMENT '交易次数',
   `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
   `update_time` datetime(0) NULL DEFAULT NULL COMMENT '修改时间',
-  `deleted` varchar(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '退市删除',
+  `deleted` varchar(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '退市删除，0表示删除',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `code`(`code`) USING BTREE COMMENT '唯一索引'
-) ENGINE = InnoDB AUTO_INCREMENT = 101394 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '股票信息表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 101909 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '股票信息表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for trading_record
@@ -143,11 +140,10 @@ CREATE TABLE `trading_record`  (
   `income_rate` decimal(32, 4) NULL DEFAULT NULL COMMENT '收益率',
   `hold_days` int(0) NULL DEFAULT NULL COMMENT '持有天数',
   `daily_income_rate` decimal(32, 4) NULL DEFAULT NULL COMMENT '日收益率',
-  `strategy_id` bigint(0) NULL DEFAULT NULL COMMENT '选股策略',
-  `strategy_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_german2_ci NULL DEFAULT NULL COMMENT '选股策略',
   `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
   `update_time` datetime(0) NULL DEFAULT NULL COMMENT '修改时间',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 222 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_german2_ci COMMENT = '交易记录表' ROW_FORMAT = Dynamic;
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `uidx_coe_buy_sale_no`(`code`, `buy_no`, `sale_no`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 474 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_german2_ci COMMENT = '交易记录表' ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
