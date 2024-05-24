4e03638a053662f127fa787978fd4f1a7c2992b3
mwangli <1325533186@qq.com>
2024-05-24 14:10:45 +0800

    股票模型评分规则优化

--- src/main/java/online/mwang/stockTrading/schedule/jobs/RunPredictJob.java
+++ src/main/java/online/mwang/stockTrading/schedule/jobs/RunPredictJob.java
@@ -19,8 +19,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicReference;
-import java.util.stream.Collectors;
 
 /**
  * @author 13255
@@ -59,34 +57,36 @@ public class RunPredictJob extends BaseJob {
             predictPrice.setDate(date);
             predictPrices.add(predictPrice);
             log.info("当前股票[{}-{}],{}预测价格为:{}", predictPrice.getCode(), predictPrice.getName(), date, predictPrice.getPrice1());
+            mongoTemplate.remove(new Query(Criteria.where("date").is(date).and("code").is(stockInfo.getCode())), VALIDATION_COLLECTION_NAME);
+            mongoTemplate.insert(predictPrices, VALIDATION_COLLECTION_NAME);
+            ModelInfo modelInfo = modelInfos.stream().filter(m -> m.getCode().equals(stockInfo.getCode())).findFirst().orElse(new ModelInfo());
+            updateStockScore(stockInfo, modelInfo);
         }
-        mongoTemplate.remove(new Query(Criteria.where("date").is(date)), VALIDATION_COLLECTION_NAME);
-        mongoTemplate.insert(predictPrices, VALIDATION_COLLECTION_NAME);
         log.info("共写入{}条股票价格预测数据。", predictPrices.size());
-        updateStockScore(stockInfos, modelInfos);
     }
 
-    private void updateStockScore(List<StockInfo> stockInfos, List<ModelInfo> modelInfos) {
-        // 获取预测结果中日期大于等于今天的数据(2条)
-        Query query = new Query(Criteria.where("date").gte(DateUtils.format1(new Date()))).with(Sort.by(Sort.Direction.DESC, "date"));
+    private void updateStockScore(StockInfo stockInfo, ModelInfo modelInfo) {
+        // 获取预测结果集中的最后10条数据
+        Query query = new Query(Criteria.where("code").is(stockInfo.getCode())).with(Sort.by(Sort.Direction.DESC, "date")).limit(10);
         List<StockPrices> predictPriceList = mongoTemplate.find(query, StockPrices.class, VALIDATION_COLLECTION_NAME);
-        ArrayList<StockInfo> updateList = new ArrayList<>();
-        predictPriceList.stream().collect(Collectors.groupingBy(StockPrices::getCode)).forEach((code, pricesList) -> {
-            if (pricesList.size() >= 2) {
-                Double curPrice = pricesList.get(0).getPrice1();
-                Double prePrice = pricesList.get(1).getPrice1();
-                double increaseRate = prePrice == 0 ? 0 : (curPrice - prePrice) / prePrice;
-                double score2 = increaseRate * 100 * 10;
-                AtomicReference<Double> score1 = new AtomicReference<>((double) 0);
-                modelInfos.stream().filter(m -> m.getCode().equals(code)).findFirst().ifPresent(m -> score1.set(m.getScore()));
-                stockInfos.stream().filter(s -> s.getCode().equals(code)).findFirst().ifPresent(s -> {
-                    s.setScore(score1.get() * 0.5 + score2 * 0.5);
-                    updateList.add(s);
-                });
+        // 判断最近10次的最大连续增长次数和总增长次数，连续增长次数越多，则说明收益越稳定
+        int totalIncreaseCount = 0, continuousIncreaseCount = 0, maxIncreaseCount = 0;
+        for (int i = 0; i < predictPriceList.size(); i++) {
+            Double curPrice = predictPriceList.get(i).getPrice1();
+            Double prePrice = predictPriceList.get(i + 1).getPrice1();
+            if (curPrice > prePrice) {
+                totalIncreaseCount++;
+                continuousIncreaseCount++;
+                maxIncreaseCount = Math.max(maxIncreaseCount, continuousIncreaseCount);
+            } else {
+                continuousIncreaseCount = 0;
             }
-        });
-        stockInfoService.updateBatchById(updateList);
-        log.info("共更新{}条股票评分数据。", updateList.size());
+        }
+        double score = (double) totalIncreaseCount * 10 + maxIncreaseCount * 10;
+        // 将价格预测评分，和模型准确率评分的加权和作为最终评分
+        Double finalScore = score * 0.8 + modelInfo.getScore() * 0.2;
+        stockInfo.setScore(finalScore);
+        stockInfoService.updateById(stockInfo);
     }
 }
 
