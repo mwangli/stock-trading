c77b5ac400e62ad26771c646e2c0cdac72a18ff8
mwangli <1325533186@qq.com>
2024-06-17 13:52:47 +0800

    预测结果展示优化

--- src/main/java/online/mwang/stockTrading/schedule/jobs/RunBuyJob.java
+++ src/main/java/online/mwang/stockTrading/schedule/jobs/RunBuyJob.java
@@ -23,7 +23,6 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Random;
 import java.util.concurrent.CountDownLatch;
 
 /**
@@ -88,16 +87,10 @@ public class RunBuyJob extends BaseJob {
                 .eq(StockInfo::getDeleted, "1").eq(StockInfo::getPermission, "1")
                 .orderByDesc(StockInfo::getScore);
         priceRanges.forEach(range -> queryWrapper.ge(StockInfo::getPrice, range[0]).le(StockInfo::getPrice, range[1]).or());
-        // 从前100条数据中分页随机选取10条
-        ArrayList<StockInfo> buyStockList = new ArrayList<>();
-        for (int i = 0; i < THREAD_COUNT * NEED_COUNT; i++) {
-            List<StockInfo> stockInfoList = stockInfoMapper.selectPage(Page.of(i, 10), queryWrapper).getRecords();
-            StockInfo stockInfo = stockInfoList.get(new Random().nextInt(10));
-            buyStockList.add(stockInfo);
-        }
+        List<StockInfo> stockInfoList = stockInfoMapper.selectPage(Page.of(1, 10), queryWrapper).getRecords();
         // 多支股票并行买入
         CountDownLatch countDownLatch = new CountDownLatch(NEED_COUNT);
-        buyStockList.forEach(s -> {
+        stockInfoList.forEach(s -> {
             sleepUtils.second(WAITING_SECONDS / THREAD_COUNT);
             cachedThreadPool.submit(() -> buyStock(s, availableAmount, countDownLatch));
         });
