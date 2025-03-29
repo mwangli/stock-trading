7378ce26ae34b2744f6895e1998e1381e63124d9
mwangli <1325533186@qq.com>
2025-03-29 16:31:04 +0800

    代码优化

--- src/main/java/online/mwang/stockTrading/web/listener/QuartzJobListener.java
+++ src/main/java/online/mwang/stockTrading/web/listener/QuartzJobListener.java
@@ -11,6 +11,7 @@ import org.quartz.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.http.ResponseEntity;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 
