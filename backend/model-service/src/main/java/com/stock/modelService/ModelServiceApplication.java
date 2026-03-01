2afd241ebc04edfb1d6835186d9a5e6ea6471546
mwangli <1325533186@qq.com>
2026-03-01 23:10:48 +0800

    fix(model-service): 修复编译错误，添加EntityScan配置
    - 修复LstmTrainerService中evaluateModel方法的重复代码
    - 修复StockLSTMModel中dropout调用错误
    - 添加TranslateException异常声明
    - 添加EntityScan注解扫描JPA实体类
    - 修正JPA仓库扫描路径为mysql子包
    - 添加RestTemplate Bean配置

--- backend/model-service/src/main/java/com/stock/modelService/ModelServiceApplication.java
+++ backend/model-service/src/main/java/com/stock/modelService/ModelServiceApplication.java
@@ -2,6 +2,7 @@ package com.stock.modelService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -19,11 +20,14 @@ import org.springframework.web.client.RestTemplate;
     "com.stock.modelService",
     "com.stock.dataCollector"
 })
+@EntityScan(basePackages = {
+    "com.stock.dataCollector.entity"
+})
 @EnableMongoRepositories(basePackages = {
     "com.stock.dataCollector.repository"
 })
 @EnableJpaRepositories(basePackages = {
-    "com.stock.dataCollector.repository"
+    "com.stock.dataCollector.repository.mysql"
 })
 public class ModelServiceApplication {
 
